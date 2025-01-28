85c08aa0b875d6bc9545044648bdd6d40a5acaad
ViniciusOliveiraDev <viniciuscontato2024@gmail.com>
2025-01-28 19:10:15 -0300

    teste de commit pelo terminal do intellij

---
+++ APIfilmes/src/main/java/com/senac/APIfilmes/ApIfilmesApplication.java
@@ -0,0 +1,13 @@
+package com.senac.APIfilmes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApIfilmesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApIfilmesApplication.class, args);
+	}
+
+}
---
+++ APIfilmes/src/test/java/com/senac/APIfilmes/ApIfilmesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senac.APIfilmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApIfilmesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
